57788502daa052c60fc4df427379006d5620077b
jattanjie21 <laboyjatta@gmail.com>
2022-09-22 17:28:49 +0000

    Reconfig graphql setup

---
+++ config/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+from django.views import defaults as default_views
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+from graphene_django.views import GraphQLView
+
+urlpatterns = [
+    path("", csrf_exempt(GraphQLView.as_view(graphiql=True))),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+if settings.DEBUG:
+    if "debug_toolbar" in settings.INSTALLED_APPS:
+        import debug_toolbar
+
+        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
